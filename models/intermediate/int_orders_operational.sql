dbbe7d74ec0ab771c18e21dd4469f7d4060ba7a5
Thierry Coulier <thierry.coulier@gmail.com>
2026-02-04 19:03:53 +0000

    Correction des modèles et application du modèle finance

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,13 +1,17 @@
-SELECT 
-margin.orders_id
-,margin.date_date
-,ROUND(SUM(margin.margin + ship.shipping_fee) - SUM((ship.logcost + ship.ship_cost)),2) AS operational_margin
-,SUM(margin.quantity) AS quantity
-,SUM(margin.revenue) AS revenue
-,SUM(margin.purchase_cost) AS purchase_cost
-,SUM(margin.margin) AS tot_margin
-FROM {{ref("int_orders_margin")}} AS margin
- LEFT JOIN {{ref("stg_raw__ship")}} AS ship
- ON margin.orders_id = ship.orders_id
- GROUP BY margin.orders_id , margin.date_date
- ORDER BY margin.orders_id DESC
+ SELECT 
+o.orders_id
+,o.date_date
+,ROUND((o.margin + ship.shipping_fee) - (ship.logcost + ship.ship_cost),2) AS operational_margin
+,o.quantity AS quantity
+,o.revenue AS revenue
+,o.purchase_cost AS purchase_cost
+,o.margin AS margin
+,ship.shipping_fee
+,ship.logcost AS log_cost
+,ship.ship_cost
+-- FROM {{ref("int_orders_margin")}} AS margin
+FROM `bases-sql-485411.dbt_thierry.int_orders_margin` AS o
+--  LEFT JOIN {{ref("stg_raw__ship")}} AS ship
+LEFT JOIN `bases-sql-485411.dbt_thierry.stg_raw__ship` AS ship
+ ON o.orders_id = ship.orders_id
+ ORDER BY o.orders_id DESC
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,8 +1,29 @@
--- SELECT
--- COUNT(orders_id) AS nb_transactions
--- ,SUM(revenue) AS revenue
--- ,SUM(revenue) / COUNT(orders_id) AS avg_basket
--- ,SUM(operational_margin) AS operational_margin
--- ,SUM(purchase_cost) AS purchase_cost
+ WITH orders_per_day AS (
+   SELECT
+     date_date
+     ,COUNT(DISTINCT orders_id) AS nb_transactions
+     ,ROUND(SUM(revenue),0) AS revenue
+     ,ROUND(SUM(margin),0) AS margin
+     ,ROUND(SUM(operational_margin),0) AS operational_margin
+     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
+     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
+     ,ROUND(SUM(log_cost),0) AS log_cost
+     ,ROUND(SUM(ship_cost),0) AS ship_cost
+     ,SUM(quantity) AS quantity
+ FROM {{ref("int_orders_operational")}}
+ GROUP BY  date_date
+ )
 
---  FROM `bases-sql-485411.dbt_thierry.int_orders_operational` 
+ SELECT
+     date_date
+     , revenue
+     , margin
+     , operational_margin
+     , purchase_cost
+     , shipping_fee
+     , log_cost
+     , ship_cost
+     , quantity
+     , ROUND(revenue/NULLIF(nb_transactions, 0), 2) AS average_basket
+ FROM orders_per_day
+ ORDER BY  date_date DESC
