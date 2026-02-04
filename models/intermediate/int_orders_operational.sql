84e7607a2fa657c6ff63eaae0cfa0454c2e67a57
Thierry <thierry.coulier@gmail.com>
2026-02-04 15:59:30 +0000

    debut du calcul de finance

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,7 +3,9 @@ margin.orders_id
 ,margin.date_date
 ,ROUND(SUM(margin.margin + ship.shipping_fee) - SUM((ship.logcost + ship.ship_cost)),2) AS operational_margin
 ,SUM(margin.quantity) AS quantity
-
+,SUM(margin.revenue) AS revenue
+,SUM(margin.purchase_cost) AS purchase_cost
+,SUM(margin.margin) AS tot_margin
 FROM {{ref("int_orders_margin")}} AS margin
  LEFT JOIN {{ref("stg_raw__ship")}} AS ship
  ON margin.orders_id = ship.orders_id
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,8 @@
+-- SELECT
+-- COUNT(orders_id) AS nb_transactions
+-- ,SUM(revenue) AS revenue
+-- ,SUM(revenue) / COUNT(orders_id) AS avg_basket
+-- ,SUM(operational_margin) AS operational_margin
+-- ,SUM(purchase_cost) AS purchase_cost
+
+--  FROM `bases-sql-485411.dbt_thierry.int_orders_operational` 
