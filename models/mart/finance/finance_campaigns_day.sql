7e75e9db6c48bba6c544133aece119a5c4450669
Thierry Coulier <thierry.coulier@gmail.com>
2026-02-05 15:48:08 +0000

    creation de finance campaigns sdays

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,19 @@
+SELECT
+f. date_date
+,f.operational_margin - c.ads_cost AS ads_margin
+,f.average_basket
+,f.operational_margin
+,c.ads_cost
+,c.impression
+,c.click
+,f.quantity
+,f.revenue
+,f.purchase_cost
+,f.margin
+,f.shipping_fee
+,f.log_cost
+,f.ship_cost
+
+ FROM {{ref("finance_days")}} AS f
+ LEFT JOIN {{ref("int_campaigns_day")}} AS c
+ ON f.date_date = c.date_date
